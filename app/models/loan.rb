1bc1942bdc326df13fea37d4fdd49e459611b72a
Emerick94 <lucas.emerick94@gmail.com>
2020-08-07 00:41:33 -0300

    add validations and fix factory accordingly

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,12 @@
 class Loan < ApplicationRecord
+  validates :name, presence: true
+  validates_length_of :cpf, is: 11
+  validates :birthdate, presence: true
+  validates :amount, presence: true
+  validates :terms, inclusion: { in: [6, 9, 12] }
+  validates :income, presence: true
+
+  before_validation do
+    self.cpf = cpf.gsub(/\D/, '')
+  end
 end
--- spec/factories/loans.rb
+++ spec/factories/loans.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :loan do
-    name { "MyString" }
-    cpf { "MyString" }
-    birthdate { "2020-08-05" }
-    amount { "9.99" }
-    terms { 1 }
-    income { "9.99" }
+    sequence(:name) { |n| "User Name ##{n}" }
+    sequence(:cpf, 10_000_000_000) { |n| n.to_s }
+    birthdate { '2020-08-05' }
+    amount { '9.99' }
+    terms { [6, 9, 12].sample }
+    income { '9.99' }
   end
 end
