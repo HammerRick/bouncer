baf716f46b4a48da94667b626639db07cc72fb1b
Emerick94 <lucas.emerick94@gmail.com>
2020-08-07 00:40:57 -0300

    add test for expected behavior on loan validations

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:loan) { build :loan }
+
+    it 'is valid with valid values' do
+      expect(loan).to be_valid
+    end
+
+    it 'is invalid with blank name' do
+      loan.name = ''
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ name: ["can't be blank"] })
+    end
+
+    it 'is invalid being to short for a cpf' do
+      loan.cpf = '961.016.146-4'
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ cpf: ['is the wrong length (should be 11 characters)'] })
+    end
+
+    it 'is invalid being to long for a cpf' do
+      loan.cpf = '961.016.146-412'
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ cpf: ['is the wrong length (should be 11 characters)'] })
+    end
+
+    it 'is valid with a valid cpf regardeless of dots and slashes, as long as the digit count is right' do
+      loan.cpf = '961............................016--------.146-41'
+      expect(loan).to be_valid
+    end
+
+    it 'is invalid with blank birthdate' do
+      loan.birthdate = ''
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ birthdate: ["can't be blank"] })
+    end
+
+    it 'is invalid without amount' do
+      loan.amount = nil
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ amount: ["can't be blank"] })
+    end
+
+    it 'is invalid without terms' do
+      loan.terms = nil
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ terms: ['is not included in the list'] })
+    end
+
+    it 'is invalid with invalid terms' do
+      loan.terms = 7
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ terms: ['is not included in the list'] })
+    end
+
+    it 'is invalid without income' do
+      loan.income = nil
+      expect(loan).to be_invalid
+      expect(loan.errors.messages).to eq({ income: ["can't be blank"] })
+    end
+  end
 end
