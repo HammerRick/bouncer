3b59d03fe250e7310752a1faaa865f888d743d00
Emerick94 <lucas.emerick94@gmail.com>
2020-08-08 17:28:52 -0300

    add validations and update factory accordingly

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,12 +1,14 @@
 class Loan < ApplicationRecord
   validates :name, presence: true
   validates_length_of :cpf, is: 11
-  validates :birthdate, presence: true
-  validates :amount, presence: true
-  validates :terms, inclusion: { in: [6, 9, 12] }
-  validates :income, presence: true
+  validates :birthdate, presence: { message: "invalid, please follow this pattern: 'yyyy-mm-dd'" }
+  validates :terms, inclusion: { in: [6, 9, 12], message: 'can only be 6, 9 or 12' }
+  validates_numericality_of :income
+  validates_numericality_of :amount, greater_than_or_equal_to: 1000,
+                                     less_than_or_equal_to: 4000,
+                                     message: "Must be a number between '1000.00' and '4000.00'"
 
   before_validation do
-    self.cpf = cpf.gsub(/\D/, '')
+    self.cpf = cpf.gsub(/\D/, '') unless cpf.nil?
   end
 end
--- spec/factories/loans.rb
+++ spec/factories/loans.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :loan do
     sequence(:name) { |n| "User Name ##{n}" }
     sequence(:cpf, 10_000_000_000) { |n| n.to_s }
-    birthdate { '2020-08-05' }
-    amount { '5000.40' }
+    birthdate { '2020-08-23' }
+    amount { '3000.40' }
     terms { [6, 9, 12].sample }
     income { '1200.55' }
   end
