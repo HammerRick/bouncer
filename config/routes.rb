dd9fa5bd2a5ba000582ceb602a40865f0c23d7bb
Emerick94 <lucas.emerick94@gmail.com>
2020-08-09 00:39:57 -0300

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :loans, only: [:index, :show, :create]
+      resources :loans, only: %i[index show create]
     end
   end
 end
