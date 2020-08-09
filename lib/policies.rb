209227b253be006cda5541a9d85179c6f9e3977e
Emerick94 <lucas.emerick94@gmail.com>
2020-08-09 14:16:26 -0300

    implement api calls for score and commitment policies

--- lib/policies.rb
+++ lib/policies.rb
@@ -1,5 +1,46 @@
 module Policies
+  include HTTParty
+
   def age_policy
-    age >= 18
+    self.refused_policy = 'age' if age < 18
+  end
+
+  def score_policy
+    # interest rate = commitment
+    self.refused_policy = 'score' if score < 600
+  end
+
+  def commitment_policy
+    self.refused_policy = 'commitment' if monthly_installment > income * commitment
+  end
+
+  def score
+    response = HTTParty.post(
+      ENV.fetch('API_URL') + + '/score',
+      headers: {
+        "x-api-key": ENV.fetch('API_KEY')
+      },
+      body: {
+        "cpf": cpf
+      }.to_json
+    )
+
+    return nil if response.code != 200
+
+    JSON.parse(response.body)['score']
+  end
+
+  def commitment
+    response = HTTParty.post(
+      ENV.fetch('API_URL') + + '/commitment',
+      headers: {
+        'x-api-key' => ENV.fetch('API_KEY')
+      },
+      body: { cpf: cpf }.to_json
+    )
+
+    return nil if response.code != 200
+
+    JSON.parse(response.body)['commitment']
   end
 end
