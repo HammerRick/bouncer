6d87f7d1292e3416ed4d4c5937ea7703abcc03f4
Emerick94 <lucas.emerick94@gmail.com>
2020-08-09 00:41:29 -0300

    inprove factory to simplify testing age policy

--- spec/factories/loans.rb
+++ spec/factories/loans.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :loan do
     sequence(:name) { |n| "User Name ##{n}" }
     sequence(:cpf, 10_000_000_000) { |n| n.to_s }
-    birthdate { '2020-08-23' }
+    birthdate { Date.today - rand(17.years..19.years) }
     amount { '3000.40' }
     terms { [6, 9, 12].sample }
     income { '1200.55' }
