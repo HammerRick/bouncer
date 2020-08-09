d3602458446b96793e42515ae10882b7facd588f
Emerick94 <lucas.emerick94@gmail.com>
2020-08-09 00:37:47 -0300

    change render to match expected results

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -10,7 +10,8 @@ class Api::V1::LoansController < ApplicationController
 
   # GET /loans/1
   def show
-    render json: @loan
+    loan_info = @loan.as_json(only: %i[id status result refused_policy amount approved_terms])
+    render json: loan_info
   end
 
   # POST /loans
@@ -18,20 +19,21 @@ class Api::V1::LoansController < ApplicationController
     @loan = Loan.new(loan_params)
 
     if @loan.save
-      render json: @loan, status: :created, location: @loan
+      render json: { id: @loan.id }, status: :created, location: api_v1_loans_url
     else
-      render json: @loan.errors, status: :unprocessable_entity
+      render json: { errors: @loan.errors.messages }, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_loan
-      @loan = Loan.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def loan_params
-      params.require(:loan).permit(:name, :cpf, :birthdate, :amount, :terms, :income)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_loan
+    @loan = Loan.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def loan_params
+    params.require(:loan).permit(:name, :cpf, :birthdate, :amount, :terms, :income)
+  end
 end
